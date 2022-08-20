7f10561ce7f4f4881c7e2bfaeaec100329582ec8
dime.thecure <uleyrus@yandex.ru>
2022-08-20 10:27:01 +0300

    20.08.2022 место Россия c Java третий ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,24 +3,24 @@ function handleProfilePopupSubmitButton(evt) {
   evt.preventDefault();
   profileTitle.textContent = profilePopupName.value;
   profileSubTitle.textContent = profilePopupAbout.value;
-  OpenPopup(profilePopup);
+  openPopup(profilePopup);
 }
 
 //нажатие на Создать на форме нового места
 function handleNewItemPopupSubmitButton(evt) {
   evt.preventDefault();
-  const newEl = AddNewElement(newItemPopup.querySelector('#newItem-about').value, newItemPopup.querySelector('#newItem-name').value);
+  const newEl = addNewElement(newItemPopup.querySelector('#newItem-about').value, newItemPopup.querySelector('#newItem-name').value);
   elementsContainer.prepend(newEl);
-  OpenPopup(newItemPopup);
+  openPopup(newItemPopup);
   newItemPopup.querySelector('#form-newItem').reset();
 }
 
 //добавление нового элемента по двум параметрам
-function AddNewElement(ElLink, ElName) {
-  const newEl = TempItem.cloneNode(true);
-  newEl.querySelector('.elements__photo').src = ElLink;
-  newEl.querySelector('.elements__photo').alt = 'Фото ' + ElName;
-  newEl.querySelector('.elements__title').textContent = ElName;
+function addNewElement(elLink, elName) {
+  const newEl = tempItem.cloneNode(true);
+  newEl.querySelector('.elements__photo').src = elLink;
+  newEl.querySelector('.elements__photo').alt = 'Фото ' + elName;
+  newEl.querySelector('.elements__title').textContent = elName;
   newEl.querySelector('.elements__like').addEventListener('click', function (evt) {
     evt.target.classList.toggle('elements__like_active');
   });
@@ -30,13 +30,14 @@ function AddNewElement(ElLink, ElName) {
   newEl.querySelector('.elements__photo').addEventListener('click', function (evt) {
     popupImagePicture.src = evt.target.src;
     popupImageTitle.textContent = evt.target.parentNode.querySelector('.elements__title').textContent;
-    OpenPopup(popupImage);
+    popupImagePicture.alt = 'Фото ' + popupImageTitle.textContent;
+    openPopup(popupImage);
   });
   return newEl;
 }
 
 //открытие и закрытие попап
-function OpenPopup(popup) {
+function openPopup(popup) {
   popup.classList.toggle('popup_opened');
 }
 
@@ -50,11 +51,11 @@ const newItemPopup = document.querySelector('#newItem');
 const popupImage = document.querySelector('#popupImage');
 const popupImagePicture = popupImage.querySelector('.popup__image');
 const popupImageTitle = popupImage.querySelector('.popup__title');
-const TempItem = document.querySelector('#element').content.querySelector('.elements__element');
+const tempItem = document.querySelector('#element').content.querySelector('.elements__element');
 
 //заполняем карточки по константам
 for (let i = 0; i < initialCards.length; i++){
-  const newElement = AddNewElement(initialCards[i].link, initialCards[i].name);
+  const newElement = addNewElement(initialCards[i].link, initialCards[i].name);
   elementsContainer.append(newElement);
 }
 
@@ -62,12 +63,12 @@ for (let i = 0; i < initialCards.length; i++){
 document.querySelector('.profile__edit-button').addEventListener('click', function () {
   profilePopupName.value = profileTitle.textContent;
   profilePopupAbout.value = profileSubTitle.textContent;
-  OpenPopup(profilePopup);
+  openPopup(profilePopup);
 });
 
 //нажатие на крестик закрытия на форме профиля
 profilePopup.querySelector('.popup__close-button').addEventListener('click', function () {
-  OpenPopup(profilePopup);
+  openPopup(profilePopup);
 });
 
 //нажатие на Сохранить на форме профиля
@@ -75,12 +76,12 @@ profilePopup.querySelector('#form-profile').addEventListener('submit', handlePro
 
 //нажатие на кнопку добавления нового места на странице
 document.querySelector('.profile__add-button').addEventListener('click', function () {
-  OpenPopup(newItemPopup);
+  openPopup(newItemPopup);
 });
 
 //нажатие на крестик закрытия на форме нового места
 newItemPopup.querySelector('.popup__close-button').addEventListener('click', function () {
-  OpenPopup(newItemPopup);
+  openPopup(newItemPopup);
 });
 
 //нажатие на Создать на форме нового места
@@ -88,5 +89,5 @@ newItemPopup.querySelector('#form-newItem').addEventListener('submit', handleNew
 
 //нажатие на крестик закрытия на открытой картинке
 popupImage.querySelector('.popup__close-button').addEventListener('click', function () {
-  OpenPopup(popupImage);
+  openPopup(popupImage);
 });
